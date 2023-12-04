821a0538503f4daa9ca3e0bd8e4abe6cecbb7eeb
Ekrem Can Lale <ekremlale1907@gmail.com>
2023-12-04 04:48:38 +0300

    form capture values from store

--- MultiSliceCourseApp/src/components/CourseForm.jsx
+++ MultiSliceCourseApp/src/components/CourseForm.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+//input degisikliklerini storea iletme
+import { useSelector, useDispatch } from "react-redux";
+import {
+  changeName,
+  changeDescription,
+  changeCost,
+} from "../store/slices/formSlice";
 
 function CourseForm() {
-  return <div></div>;
+  const dispatch = useDispatch();
+
+  const { name, description, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      description: state.form.description,
+      cost: state.form.cost,
+    };
+  });
+  // console.log(name, description, cost);
+  return (
+    <div className="courseForm panel">
+      <h4 className="subtitle is-3">Kurs Ekle</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Ad</label>
+            <input
+              className="input is-expanded"
+              onChange={(e) => {
+                dispatch(changeName(e.target.value));
+              }}
+              //basma işlemi value ile
+              value={name}
+            />
+          </div>
+          <div className="field">
+            <label className="label">Açıklama</label>
+            <textarea
+              className="input is-expanded"
+              onChange={(e) => {
+                dispatch(changeDescription(e.target.value));
+              }}
+              value={description}
+            />
+          </div>
+          <div className="field">
+            <label className="label">Fiyat</label>
+            <input
+              className="input is-expanded"
+              type="number"
+              onChange={(e) => {
+                dispatch(changeCost(parseInt(e.target.value)));
+              }}
+              value={cost}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-primary">Kaydet</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CourseForm;
