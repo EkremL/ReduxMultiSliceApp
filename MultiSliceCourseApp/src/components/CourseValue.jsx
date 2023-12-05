8605dc7ce21a25d1ec202c982c89f0c8876287af
Ekrem Can Lale <ekremlale1907@gmail.com>
2023-12-06 02:10:57 +0300

    reduce

--- MultiSliceCourseApp/src/components/CourseValue.jsx
+++ MultiSliceCourseApp/src/components/CourseValue.jsx
@@ -4,15 +4,22 @@ import { useSelector } from "react-redux";
 function CourseValue() {
   const totalCost = useSelector(({ form, courses: { data, searchTerm } }) => {
     //destructoring yapiyoruz
-    const filteredCourses = data.filter((course) =>
-      course.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    // const filteredCourses = data.filter((course) =>
+    //   course.name.toLowerCase().includes(searchTerm.toLowerCase())
+    // );
 
-    let cost = 0;
-    for (let course of filteredCourses) {
-      cost += course.cost;
-    }
-    return cost;
+    // let cost = 0;
+    // for (let course of filteredCourses) {
+    //   cost += course.cost;
+    // }
+    // return cost;
+    //bu kısım doğru fakat daha güzel bir kullanim yapılabilir
+    ///////////////////////////////////// alttaki de alternatif bir kullanımdır
+    return data
+      .filter((course) =>
+        course.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .reduce((acc, course) => acc + course.cost, 0);
   });
 
   return <div className="coursePrice">Toplam Tutar: {totalCost} TL</div>;
